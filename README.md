62ff7414d579bd6120a1bee3a8215777f85e08fc
3DLIRIOUS <3dlirious@gmail.com>
2016-07-24 12:25:30 -0500

    Updated version number to 20160724 in setup.py Added additional fields to setup.py Added manifest & README files

---
+++ README.md
@@ -0,0 +1,7 @@
+
+#PyLirious
+
+3D model creation and processing with Python.
+
+#License
+[LGPL version 2.1] (http://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html)
--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup
 
 setup(name='pylirious',
-      version='20160518',
-      description='Crazy 3D model creation and processing with Python',
+      version='20160724',
+      description='3D model creation and processing with Python',
+      url='https://github.com/3DLIRIOUS/pylirious',
       author='3DLirious',
       author_email='3DLirious@gmail.com',
       license='LGPL-2.1',
-      packages=['pylirious'])
+      packages=['pylirious'],
+      install_requires=['meshlabxml',])
