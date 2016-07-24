8218331b327342b328ed97e3bd33df58727024bb
3DLIRIOUS <3dlirious@gmail.com>
2016-07-24 14:03:42 -0500

    Updated packaging

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup
 
 setup(name='pylirious',
-      version='20160724',
+      version='20160724c',
       description='3D model creation and processing with Python',
       url='https://github.com/3DLIRIOUS/pylirious',
       author='3DLirious',
       author_email='3DLirious@gmail.com',
       license='LGPL-2.1',
       packages=['pylirious'],
-      install_requires=['meshlabxml',])
+      install_requires=['meshlabxml',],
+      include_package_data=True)
