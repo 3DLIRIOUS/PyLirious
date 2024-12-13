a983de4705d79232f635a04e0c9bde9b4aec2aaa
3DLirious <3DLirious@gmail.com>
2024-12-13 09:39:21 -0600

    Update setup_exe_paths.py

--- pylirious/setup_exe_paths.py
+++ pylirious/setup_exe_paths.py
@@ -27,53 +27,53 @@ import sys
 import shutil
 import platform
 
+# TODO: add additional platform specific default locations,
+# e.g. Linux & MacOS
+if platform.system() == 'Windows':
+    #meshlabserver_path = 'C:\\Program Files\\VCG\\MeshLab'
+    meshlabserver_path = 'K:\\shared\\software\\3D\\MeshLab\\MeshLab2020.09-windows'
+    openscad_path = 'C:\\Program Files\\OpenSCAD'
+    admesh_path = 'C:\\Program Files\\admesh'
+    #blender_path = 'C:\\Program Files\\Blender Foundation\\Blender'
+    blender_path = 'K:\\shared\\software\\3D\\Blender\\Blender_Portable\\blender-2.79b-windows64'
+    meshmixer_path = 'C:\\Program Files\\Autodesk\\Meshmixer'
+    inkscape_path = 'C:\\Program Files\\Inkscape'
+    pstoedit_path = 'C:\\Program Files\\pstoedit'
+
 
 def add2path(newpath):
     """Add newpath to the system PATH environment variable."""
     if newpath not in os.environ['PATH']:
+        #os.environ['PATH'] = newpath + os.pathsep + os.environ['PATH']
         os.environ['PATH'] += os.pathsep + newpath
 
 
 def setup_exe_paths():
-    """Add external dependencies to the system path
-
-    Currently supports the following programs on Win64:
-        meshlabserver
-        openscad
-        admesh
-        blender
-        meshmixer
-
-    """
-    # TODO: add additional platform specific default locations,
-    # e.g. Linux & MacOS
+    """Add external dependencies to the system path."""
     if sys.version.split()[0] >= '3.3':
         if shutil.which('meshlabserver') is None:
-            if platform.system() == 'Windows':
-                add2path('C:\\Program Files\\VCG\\MeshLab')
+            add2path(meshlabserver_path)
         if shutil.which('openscad') is None:
-            if platform.system() == 'Windows':
-                add2path('C:\\Program Files\\OpenSCAD')
+            add2path(openscad_path)
         if shutil.which('admesh') is None:
-            if platform.system() == 'Windows':
-                add2path('C:\\Program Files\\admesh')
+            add2path(admesh_path)
         if shutil.which('blender') is None:
-            if platform.system() == 'Windows':
-                add2path('C:\\Program Files\\Blender Foundation\\Blender')
+            add2path(blender_path)
         if shutil.which('meshmixer') is None:
-            if platform.system() == 'Windows':
-                add2path('C:\\Program Files\\Autodesk\\Meshmixer')
+            add2path(meshmixer_path)
         if shutil.which('inkscape') is None:
-            if platform.system() == 'Windows':
-                add2path('C:\\Program Files\\Inkscape')
+            add2path(inkscape_path)
+        if shutil.which('pstoedit') is None:
+            add2path(pstoedit_path)
     else:
         # If shutil.which is not available, skip checking path
-        add2path('C:\\Program Files\\VCG\\MeshLab')
-        add2path('C:\\Program Files\\OpenSCAD')
-        add2path('C:\\Program Files\\admesh')
-        add2path('C:\\Program Files\\Blender Foundation\\Blender')
-        add2path('C:\\Program Files\\Autodesk\\Meshmixer')
-        add2path('C:\\Program Files\\Inkscape')
+        add2path(meshlabserver_path)
+        add2path(openscad_path)
+        add2path(admesh_path)
+        add2path(blender_path)
+        add2path(meshmixer_path)
+        add2path(inkscape_path)
+        add2path(pstoedit_path)
 
 
 if __name__ == "__main__":
